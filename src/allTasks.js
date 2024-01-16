f970cce09351f6df89161c416e418e22ebeffc73
Chris <129601042+Operations-Department@users.noreply.github.com>
2024-01-16 16:52:46 +0900

    Add eventListener to edit task description, due date, and priority after creation

--- src/allTasks.js
+++ src/allTasks.js
@@ -119,21 +119,24 @@ function isIncomplete(task, taskContainer, event, domTitle, domDescription) {
     domDescription.classList.remove('completed-task-text');
 }
 
-
-
-
 //edit task after creation
-bodyContentContainer.addEventListener('click', function(event) {
+bodyContentContainer.addEventListener('dblclick', function(event) {
 
+    //identify elements
     const taskContainer = event.target.closest('.task-container');
     const taskElementLeft = event.target.closest('.task-element-left');
+    const taskElementRight = event.target.closest('.task-element-right');
     const taskTitle = event.target.closest('.task-title');
-    
-    if (taskTitle) {
-        //create element
-        const editInputBox = document.createElement('input');
-        editInputBox.classList.add('form-title');
-        
+    const taskDescription = event.target.closest('.task-description');
+    const taskDueDate = event.target.closest('.task-due-date');
+    const taskPriority = event.target.closest('.task-priority');
+
+    //create element
+    const editInputBox = document.createElement('input');
+    editInputBox.classList.add('form-title');
+
+    //edit the task title
+    if (taskTitle) {        
         const taskIDNumber = taskContainer.getAttribute('data-task-id');
 
         //add input to edit title
@@ -158,5 +161,99 @@ bodyContentContainer.addEventListener('click', function(event) {
                 taskElementLeft.replaceChild(taskTitle, editInputBox);
             }
         });
+    }
+
+    //edit the task description
+    if (taskDescription) {        
+        const taskIDNumber = taskContainer.getAttribute('data-task-id');
+
+        //add input to edit title
+        taskElementLeft.replaceChild(editInputBox, taskDescription);
+        editInputBox.focus();
+
+        //take edited title and submit
+        editInputBox.addEventListener('keyup', function(event) {
+            if (event.key === 'Enter' && editInputBox.value.trim() !== '') {
+                
+                //extract values
+                const tasks = getTasks();
+                const task = tasks.find(task => task.taskID == taskIDNumber);
+
+                //update task title
+                task.description = editInputBox.value;
+
+                //update dom display
+                taskDescription.textContent = editInputBox.value;
+
+                //re-append element
+                taskElementLeft.replaceChild(taskDescription, editInputBox);
+            }
+        });
+    }
+
+    //edit the task due date
+    if (taskDueDate) {        
+        editInputBox.type = 'date';
+        const taskIDNumber = taskContainer.getAttribute('data-task-id');
+
+        //add input to edit title
+        taskElementRight.replaceChild(editInputBox, taskDueDate);
+
+        //take edited title and submit
+        editInputBox.addEventListener('change', function(event) {
+                
+            //extract values
+            const tasks = getTasks();
+            const task = tasks.find(task => task.taskID == taskIDNumber);
+
+            //update task title
+            task.dueDate = editInputBox.value;
+
+            //update dom display
+            taskDueDate.innerText = editInputBox.value;
+
+            //re-append element
+            taskElementRight.replaceChild(taskDueDate, editInputBox);
+    
+        });
+    }
+
+    //edit priority button
+    if (taskPriority) {
+
+        //extract values
+        const taskIDNumber = taskContainer.getAttribute('data-task-id');
+        const tasks = getTasks();
+        const task = tasks.find(task => task.taskID == taskIDNumber);
+
+        //cycle between low-mid-high priority
+        switch (true) {
+            case taskPriority.classList.contains('task-priority-low'):
+                taskPriority.classList.remove('task-priority-low');
+                taskContainer.classList.remove('task-container-low-priority');
+                taskPriority.classList.add('task-priority-mid');
+                taskContainer.classList.add('task-container-mid-priority');
+                task.priority = 'medium';
+                taskPriority.textContent = 'medium';
+                break;
+    
+            case taskPriority.classList.contains('task-priority-mid'):
+                taskPriority.classList.remove('task-priority-mid');
+                taskContainer.classList.remove('task-container-mid-priority');
+                taskPriority.classList.add('task-priority-high');
+                taskContainer.classList.add('task-container-high-priority');
+                task.priority = 'high';
+                taskPriority.textContent = 'high';
+                break;
+    
+            case taskPriority.classList.contains('task-priority-high'):
+                taskPriority.classList.remove('task-priority-high');
+                taskContainer.classList.remove('task-container-high-priority');
+                taskPriority.classList.add('task-priority-low');
+                taskContainer.classList.add('task-container-low-priority');
+                task.priority = 'low';
+                taskPriority.textContent = 'low';
+                break;
+        }
     }
 });
