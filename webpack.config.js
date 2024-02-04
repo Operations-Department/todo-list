40e06d60dc02e285c50702530eba797b3c3c60f3
Chris <129601042+Operations-Department@users.noreply.github.com>
2024-02-04 16:52:50 +0900

    update display - github icon, credits color, favicon

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
+            filename: 'index.html',
             favicon: './src/imgs/check.ico',
         }),
     ],
